74954b1a52cbe9683efe9f5f9e2b621a079dfa82
malabika509 <91895369+malabika509@users.noreply.github.com>
2025-03-25 16:00:50 +0530

    Add files via upload

---
+++ api_credentials.h
@@ -0,0 +1,11 @@
+#ifndef API_CREDENTIALS_H
+#define API_CREDENTIALS_H
+
+#include <cstdlib>  // For std::getenv
+#include <string>
+
+const std::string CLIENT_ID = "ba4GXxXA";
+const std::string CLIENT_SECRET = "Xkg5MpcLWnzBSXqwEYMEg4Ks-1tKVWJMSDFRblfRnYk";
+
+#endif // API_CREDENTIALS_H
+
